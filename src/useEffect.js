ec240c2015fbddcb8e37b575a0fcb2e69d2d3357
lattesmoody <lattesmoody@gmail.com>
2022-08-08 21:23:32 +0900

    #2.0 useEffect

---
+++ src/useEffect.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import ReactDom from "react-dom";
+
+import "./styles.css";
+
+export default function App() {
+  const sayHello = () => console.log("hello");
+
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, [number]);
+  return (
+    <div className="App">
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
+    </div>
+  );
+}
+// useEffect: ComponentDidMount, ComponentWillUnMount, ComponentDidUpdate
