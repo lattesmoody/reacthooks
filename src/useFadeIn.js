1a9a9fe87bedb7878f1e4052ace6ceb5a943b91a
lattesmoody <lattesmoody@gmail.com>
2022-08-09 15:06:14 +0900

    #2.5 useFadeIn

---
+++ src/useFadeIn.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDom from "react-dom";
+
+const useFadeIn = (duration = 1, delay = 0) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      const { current } = element;
+      current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
+      current.style.opacity = 1;
+    }
+  });
+  if (typeof duration !== "number" || typeof delay !== "number") {
+    return;
+  }
+  return { ref: element, style: { opacity: 0 } };
+};
+
+export default function App() {
+  const fadeInH1 = useFadeIn(1, 2);
+  const fadeInP = useFadeIn(5, 10);
+  return (
+    <div className="App">
+      <h1 {...fadeInH1}>Hello</h1>
+      <p {...fadeInP}>lattesmoody</p>
+    </div>
+  );
+}
