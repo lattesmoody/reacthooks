2c9f974f95692f89d045750d0bc6f248b17797fe
lattesmoody <lattesmoody@gmail.com>
2022-08-09 13:58:10 +0900

    #2.3 useConfirm

---
+++ src/useConfirm.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState, useRef } from "react";
+import ReactDom from "react-dom";
+
+const useConfirm = (message = "", callback, rejection) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (confirm(message)) {
+      callback();
+    } else {
+      rejection();
+    }
+  };
+  return confirmAction;
+};
+
+export default function App() {
+  const deleteWorld = () => console.log("Deleting the world");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort);
+  return (
+    <div className="App">
+      <button onClick={confirmDelete}>Delete the world</button>
+    </div>
+  );
+}
